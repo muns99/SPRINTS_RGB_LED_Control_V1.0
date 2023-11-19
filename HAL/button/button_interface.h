a545e40a3e6cee00a0a409319297cf0e6130b376
mohamed Mansour <mans75043@gmail.com>
2023-11-19 17:18:14 +0200

    add button state enum

--- HAL/button/button_interface.h
+++ HAL/button/button_interface.h
@@ -6,5 +6,13 @@ typedef enum __buttonErrorState
 
 }enu_buttonErrorState_t;
 
+typedef enum __buttonState
+{
+
+}enu_buttonState_t;
+typedef struct __button
+{
+    /* data */
+}st_button_t;
 
 #endif /*BUTTON_INTERFACE_H_*/
