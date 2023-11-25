bbf95a46eaad01e08c282d9bc0b89ff1a3426e02
mohamed Mansour <mans75043@gmail.com>
2023-11-25 13:46:33 +0200

    last commit

--- APP/app_config.h
+++ APP/app_config.h
@@ -1,11 +1,11 @@
 #ifndef APP_CONFIG_H_
 #define APP_CONFIG_H_ 
-void buttonSw1callback(void);
+void APP_sequence(void);
 st_button_t st_g_sw1 = {
     .buttonPort = BUTTON_PORTF,
     .buttonPin  = BUTTON_PIN4,
     .buttonMode = BUTTON_INTERRUPT_FALLING_EDGE_MODE,
-    buttonHandler = buttonSw1callback
+    .buttonHandler = APP_sequence
 };
 st_led_t st_g_redLed = {
     .ledPort = LED_PORTF,
--- main.c
+++ main.c
@@ -1,30 +1,13 @@
 #include "MCAL/gpio/gpio_interface.h"
 #include "HAL/button/button_interface.h"
 #include "HAL/led/led_interface.h"
-st_led_t redLed = {LED_PIN1,LED_PORTF,LED_OFF};
-void routine()
-{
-    LED_on(&redLed);
-    while (1)
-    {
-        /* code */
-    }
-    
-}
+
 int main()
 {
 	
-    st_button_t sw1 = {
-        .buttonPort = BUTTON_PORTF,
-        .buttonPin = BUTTON_PIN4,
-        .buttonMode = BUTTON_INTERRUPT_FALLING_EDGE_MODE,
-        .buttonHandler = routine
-    };
-    LED_init(&redLed);
-    LED_on(&redLed);
-    BUTTON_init(&sw1);
-    BUTTON_enable(&sw1);
-    
+
+    APP_init();
+	APP_start();
     while (1)
     {
         /* code */
