90e509cbbb54c075448fe9d9f4d6b6a2174b79a9
mohamed Mansour <mans75043@gmail.com>
2023-11-30 14:45:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
-#include "MCAL/gpio/gpio_interface.h"
-#include "HAL/button/button_interface.h"
-#include "HAL/led/led_interface.h"
+#include "APP/app_interface.h"
 #include "TM4C123.h"
 #include "core_cm4.h" 
 int main()
 {
 	
 
-		APP_init();
-		APP_start();
+	APP_init();
+	APP_start();
     while (1)
     {
     }
